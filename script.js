56f4c2c0e5c7f7c52d6050a1909aca7576537eb3
Clara <clara.gabrielle@youxgroup.com.br>
2022-07-07 18:44:33 -0300

    Adicionando Loader no projeto

--- script.js
+++ script.js
@@ -1,23 +1,37 @@
 // Get quotes from API
 
+const quoteContainer = document.getElementById('quote-container');
 const quoteText = document.getElementById('quote');
 const author = document.getElementById('author');
 const twitterBtn =  document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+function complete() {
+    loader.hidden = true;
+    quoteContainer.hidden = false;
+}
+
 function newQuote() {
+    loading();
     const randomQuotes = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
 
     !randomQuotes.author ? author.textContent = '- Unknown' : author.textContent = `- ${randomQuotes.author}`;
     randomQuotes.text.length > 150 ? quoteText.classList.add('long-quote') : quoteText.classList.remove('long-quote');
 
     quoteText.textContent = randomQuotes.text;
-
+    complete();
 }
 
 async function getQuoteAPI() {
+    loading();
     const apiUrl = 'https://type.fit/api/quotes'
     try {
         const response = await fetch(apiUrl);
